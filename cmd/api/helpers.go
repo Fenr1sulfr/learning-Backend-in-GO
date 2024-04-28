56c448dfc4c82342eb8450b340f882ee5dc27bd3
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-04-28 11:27:11 +0500

    Added helpers, as riteJSON, readJson, readParam

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]any
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
-	id, err := strconv.ParseInt(params.ByName("ID"), 10, 64)
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshallTypeError *json.UnmarshalTypeError
+		var invalidUnmarshallError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON at character %d", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return fmt.Errorf("Body contains badly-formed JSON")
+		case errors.As(err, unmarshallTypeError):
+			if unmarshallTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshallTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type for field %b", unmarshallTypeError.Offset)
+		case errors.Is(err, io.EOF):
+
+			return fmt.Errorf("body must not to be empty")
+		case errors.As(err, &invalidUnmarshallError):
+			panic(err)
+		default:
+			return err
+		}
+
+	}
+	return nil
+}
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
 }
