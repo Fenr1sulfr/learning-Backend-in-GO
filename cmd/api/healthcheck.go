f2b4cefad7346c794529a18e0f6e393afad15ac4
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-04-28 11:28:23 +0500

    Adding new helpers(readJson,writeJson) in handlers

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status":"available", "environment":%q, "version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.serverErrorRespone(w, r, err)
+	}
 }
