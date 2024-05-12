2522b9518290c49bc2d5e470aa9e15daa487a71a
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-05-12 10:55:59 +0500

    Validation of filters, struct of filters added

---
+++ internal/data/filters.go
@@ -0,0 +1,19 @@
+package data
+
+import "sulfur.test.net/internal/data/validator"
+
+type Filters struct {
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafeList []string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters) {
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum 100")
+
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
--- internal/data/models.go
+++ internal/data/models.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	ErrNoRecordFound = errors.New("record not found")
+	ErrEditConflict  = errors.New("edit conflict")
 )
 
 type Models struct {
