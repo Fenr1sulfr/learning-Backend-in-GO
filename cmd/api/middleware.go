1e5b0b8a7719d025aeaf115b756bc2cdd81c0d6e
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-07-20 16:41:51 +0500

    graceful shutdown and rate limiter added

---
+++ cmd/api/middleware.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorRespone(w, r, fmt.Errorf(`%s`, err))
+			}
+
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.config.limiter.enabled {
+		
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorRespone(w, r, err)
+			return
+		}
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			// Create and add a new client struct to the map if it doesn't already exist.
+			clients[ip] = &client{
+
+				limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+			}
+		}
+		// Update the last seen time for the client.
+		clients[ip].lastSeen = time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceedResponse(w, r)
+			return
+		}
+		mu.Unlock()
+	}
+		next.ServeHTTP(w, r)
+	})
+}
