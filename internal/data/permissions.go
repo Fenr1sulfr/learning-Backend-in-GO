0af886041e153f8f69a9890412f32894e6b8cadd
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-10-26 20:53:05 +0500

    Adding permissions for user

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
@@ -19,6 +21,17 @@ func (p Permissions) Include(code string) bool {
 
 type PermissionModel struct {
 	DB *sql.DB
+}
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code=ANY($2)
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	_, err := m.DB.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
 
 func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
